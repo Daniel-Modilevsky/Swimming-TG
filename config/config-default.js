95f357e67e06914e40e11ee7b4fe8d1d7532dce9
Daniel-Modilevsky <62332613+Daniel-Modilevsky@users.noreply.github.com>
2021-01-20 11:02:12 +0200

    port + procfile

--- config/config-default.js
+++ config/config-default.js
@@ -3,7 +3,6 @@ dotenv.config();
 
 // enviroment variable
 const config = {
-    PORT: process.env.port,
     LOGFILE: process.env.logFile,
     CS: process.env.CS,
     DB_USER: process.env.DB_USER,
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = require('./lib/express');
 const logger = require('./lib/logs');
 const config = require('./config/config-default');
 const { initConnection } = require('./lib/mongoose');
-
+let port = process.env.PORT || 8080;
 try{
     initConnection();
 
     const server = http.createServer(app);
-    server.listen(config.PORT, () => logger.info(`Lisining to Server : ${config.PORT}`));
+    server.listen(port, () => logger.info(`Lisining to Server : ${port}`));
 }
 catch(error){
     logger.error(`Server Problem - Server will not up : ${error}`);
